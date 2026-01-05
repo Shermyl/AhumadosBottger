412430bade8158f801c54ec0cebb68492219567a
mirellybarrionuevo <23121028@unamad.edu.pe>
2026-01-05 14:53:15 -0500

    Update entrada.php
    lalalalalala

--- entrada.php
+++ entrada.php
@@ -1,14 +1,11 @@
-<?php 
-    require 'includes/funciones.php';
-    incluirTemplate('header');
+<?php
+require 'includes/funciones.php';
+incluirTemplate('header');
 ?>
-
-    <main class="contenedor seccion contenido-centrado">
-        <h1>Guía para la decoración de tu hogar</h1>
-
-
-        <picture>
-            <source srcset="build/img/destacada2.webp" type="image/webp">
+<main class= "contenedor seccion contenido-centrado">
+    <h1>Guia para la decoracion de tu hogar</h1>
+    <picture>
+        <source srcset="build/img/destacada2.webp" type="image/webp">
             <source srcset="build/img/destacada2.jpg" type="image/jpeg">
             <img loading="lazy" src="build/img/destacada2.jpg" alt="imagen de la propiedad">
         </picture>
