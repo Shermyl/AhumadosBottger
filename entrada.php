424361c9606cefa2ad85b5be8effaabbfe0d2631
mirellybarrionuevo <23121028@unamad.edu.pe>
2026-01-05 15:48:12 -0500

    Update entrada.php

--- entrada.php
+++ entrada.php
@@ -3,14 +3,14 @@ require 'includes/funciones.php';
 incluirTemplate('header');
 ?>
 <main class= "contenedor seccion contenido-centrado">
-    <h1>Guia para la decoracion de tu hogar</h1>
+    <h1>Ahumados Bottger </h1>
     <picture>
         <source srcset="build/img/destacada2.webp" type="image/webp">
             <source srcset="build/img/destacada2.jpg" type="image/jpeg">
             <img loading="lazy" src="build/img/destacada2.jpg" alt="imagen de la propiedad">
         </picture>
 
-        <p class="informacion-meta">Escrito el: <span>20/10/2021</span> por: <span>Admin</span> </p>
+        <p class="informacion-meta">Escrito el: <span>20/12/2025</span> por: <span>Admin</span> </p>
 
 
         <div class="resumen-propiedad">
